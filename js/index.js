49ca58683a3ce6601f68e609974a5573cfe73495
MarcosGual <marcos.gual@outlook.com>
2024-11-28 18:30:23 -0300

    add: under construction + dark theme working

--- js/index.js
+++ js/index.js
@@ -4,3 +4,13 @@ function toggleNavMenu() {
   $menu.classList.toggle("open");
   $icon.classList.toggle("open");
 }
+
+const toggleTheme = document.querySelector('#toggleTheme');
+
+toggleTheme.addEventListener('change', () => {
+    if(toggleTheme.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+});
